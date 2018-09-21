ff0626c0594e3d0b8cb4deb39c1fabe474177c0a
Bijayini Parhi <bijayini.parhi@gmail.com>
2018-09-22 03:02:23 +0530

    #000 | Bijayini | Sorted movie solution

--- src/MovieList/index.jsx
+++ src/MovieList/index.jsx
@@ -4,7 +4,20 @@ import {Movies} from '../constants';
 
 import MovieCard from '../MovieCard';
 
-const MovieList = () => {
+const getMovieListSortedByParam = (sortingParam) => Movies.sort((movie1, movie2) => {
+  const firstMovie = movie1[sortingParam].toLowerCase();
+  const secondMovie = movie2[sortingParam].toLowerCase();
+
+  return (firstMovie > secondMovie) ? 1 : ((secondMovie > firstMovie) ? -1 : 0);
+});
+
+
+const getSortedMovieList = (sortingParam)=> {
+   return sortingParam !== '' ? getMovieListSortedByParam(sortingParam): Movies;
+}
+
+const MovieList = (props) => {
+  const sortedMovieList = getSortedMovieList(props.sortingParam);
   return(
       <ul>
         {Movies.map( movie => <MovieCard movie={movie} key={movie.id}/> )}
